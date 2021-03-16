baf5623c03629e141964394a41fbb61cb2bc8d39
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-03-16 21:18:19 +0000

    Setup MainTest

--- src/main/test/MainTest.py
+++ src/main/test/MainTest.py
@@ -1,9 +1,14 @@
 import unittest
-
+from src.main.python.ReadFromFile import ReadFromFile
+from src.main.python.TestOutput import TestOutput
+from src.main.python.TestInput import TestInput
 
 class MainTest(unittest.TestCase):
     def test_play(self):
-        self.assertEqual(True, False)
+        readFromFile = ReadFromFile()
+        recordedOutput = readFromFile.read("ConsoleOutput.csv")
+        testOutput = TestOutput()
+        self.assertEqual(recordedOutput, testOutput)
 
 
 if __name__ == '__main__':
