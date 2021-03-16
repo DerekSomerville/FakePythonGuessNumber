f730dff0a91c06a0487d11f0c51fc2c414c6ba0b
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-03-16 21:20:54 +0000

    MaintTest added Main create

--- src/main/test/MainTest.py
+++ src/main/test/MainTest.py
@@ -2,12 +2,14 @@ import unittest
 from src.main.python.ReadFromFile import ReadFromFile
 from src.main.python.TestOutput import TestOutput
 from src.main.python.TestInput import TestInput
+from src.main.python.Main import Main
 
 class MainTest(unittest.TestCase):
     def test_play(self):
         readFromFile = ReadFromFile()
         recordedOutput = readFromFile.read("ConsoleOutput.csv")
         testOutput = TestOutput()
+        main = Main()
         self.assertEqual(recordedOutput, testOutput)
 
 
