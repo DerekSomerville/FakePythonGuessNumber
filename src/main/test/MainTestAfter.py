73b9392bf8ee99e5a6953fe073c0b1aab4b2462a
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-03-17 16:08:33 +0000

    Added test of recording

--- src/main/test/MainTestAfter.py
+++ src/main/test/MainTestAfter.py
@@ -4,12 +4,24 @@ from src.main.python.TestOutput import TestOutput
 from src.main.python.TestInput import TestInput
 from src.main.python.Main import Main
 
-class MainTest(unittest.TestCase):
+class MainTestBeforeAfter(unittest.TestCase):
+
+    def getMain(self, testOutput):
+        readFromFile = ReadFromFile()
+        userTestInput = TestInput()
+        userTestData = readFromFile.read("ConsoleInput.csv")
+        userTestInput.setInputList(userTestData)
+        randomTestInput = TestInput()
+        randomTestData = readFromFile.read("RandomInput.csv")
+        randomTestInput.setInputList(randomTestData)
+        main = Main(testOutput,userTestInput,randomTestInput)
+        return main
+
     def test_play(self):
         readFromFile = ReadFromFile()
         recordedOutput = readFromFile.read("ConsoleOutput.csv")
         testOutput = TestOutput()
-        main = Main()
+        main = self.getMain(testOutput)
         main.play();
         print("Recorded Output: ",recordedOutput)
         print("TestOutput: ",testOutput.getOutputList())
---
+++ src/main/test/MainTestBefore.py
@@ -0,0 +1,20 @@
+import unittest
+from src.main.python.ReadFromFile import ReadFromFile
+from src.main.python.TestOutput import TestOutput
+from src.main.python.TestInput import TestInput
+from src.main.python.Main import Main
+
+class MainTestBefore(unittest.TestCase):
+    def test_play(self):
+        readFromFile = ReadFromFile()
+        recordedOutput = readFromFile.read("ConsoleOutput.csv")
+        testOutput = TestOutput()
+        main = Main()
+        main.play();
+        print("Recorded Output: ",recordedOutput)
+        print("TestOutput: ",testOutput.getOutputList())
+        self.assertEqual(recordedOutput, testOutput.getOutputList())
+
+
+if __name__ == '__main__':
+    unittest.main()
